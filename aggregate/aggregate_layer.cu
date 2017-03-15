a42bd3a19aa55c3d13bd74930ecc33dd17960483
Zuo Wang <zuowang@users.noreply.github.com>
2017-03-16 00:03:20 +0800

    Update aggregate_layer.cu

--- aggregate/aggregate_layer.cu
+++ aggregate/aggregate_layer.cu
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 #include <vector>
 
 #include "caffe/layers/aggregate_layer.hpp"
@@ -8,22 +16,22 @@ namespace caffe {
 template <typename Dtype>
 void AggregateLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-    if (bottom.size() == 1) { return; }
-    caffe_set(sizeof(Dtype) * top[0]->count(), Dtype(0), top[0]->mutable_gpu_data());
-    for(int i = 0;i < bottom.size();++i){
-        caffe_add(sizeof(Dtype) * bottom[i]->count(), bottom[i]->gpu_data(), top[0]->mutable_gpu_data(), top[0]->mutable_gpu_data());
-    }
+  Dtype * top_data = top[0]->mutable_gpu_data();
+  int channels = bottom[0]->channels();
+  caffe_gpu_set(top[0]->count(), Dtype(0), top[0]->mutable_gpu_data());
+  for(int i = 0; i < bottom[0]->num(); ++i){
+    caffe_gpu_add(channels, bottom[0]->gpu_data() + i*channels, top_data, top_data);
+  }
 }
 
 template <typename Dtype>
 void AggregateLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    if (bottom.size() == 1) { return; }
-    int offset = 0;
-    for(int i = 0;i < bottom.size();++i){
-        caffe_gpu_memcpy(sizeof(Dtype) * bottom[i]->count(), top[0]->gpu_diff() + offset, bottom[i]->mutable_gpu_diff());
-        offset += bottom[i]->count();
-    }
+  int channels = bottom[0]->channels();
+  Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
+  for(int i = 0;i < bottom[0]->num(); ++i){
+    caffe_gpu_memcpy(channels, top[0]->gpu_diff(), bottom_diff + i*channels);
+  }
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(AggregateLayer);
